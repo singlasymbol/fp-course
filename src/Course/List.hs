c12b162bc947af94399ecd57934ca58a85c85c41
Symbol Singla <singla.symbol@juspay.in>
2020-03-22 03:10:12 +0530

    completed the flat list fns

--- src/Course/List.hs
+++ src/Course/List.hs
@@ -188,8 +188,7 @@ infixr 5 ++
 flatten ::
   List (List a)
   -> List a
-flatten =
-  error "todo: Course.List#flatten"
+flatten = foldRight (\elem auxList ->  elem ++ auxList) Nil
 
 -- | Map a function then flatten to a list.
 --
@@ -205,8 +204,7 @@ flatMap ::
   (a -> List b)
   -> List a
   -> List b
-flatMap =
-  error "todo: Course.List#flatMap"
+flatMap fn list = foldRight (\elem auxList -> (fn elem) ++ auxList) Nil list
 
 -- | Flatten a list of lists to a list (again).
 -- HOWEVER, this time use the /flatMap/ function that you just wrote.
@@ -215,8 +213,7 @@ flatMap =
 flattenAgain ::
   List (List a)
   -> List a
-flattenAgain =
-  error "todo: Course.List#flattenAgain"
+flattenAgain = flatMap id
 
 -- | Convert a list of optional values to an optional list of values.
 --
